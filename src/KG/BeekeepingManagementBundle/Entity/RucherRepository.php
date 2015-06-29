1aa92d0155568d95ab8da345ac27cdb539cc6100
keverg <kevin.greneche@jack12.fr>
2015-06-29 19:08:24 +0200

    Compteur de rucher

--- src/KG/BeekeepingManagementBundle/Entity/RucherRepository.php
+++ src/KG/BeekeepingManagementBundle/Entity/RucherRepository.php
@@ -24,4 +24,13 @@ class RucherRepository extends EntityRepository
         
         return new Paginator($q);
     }
+    
+    public function getNbRucherTotal()
+    {
+        return $this->_em->createQueryBuilder()
+                ->select('count(rucher.id)')
+                ->from('KGBeekeepingManagementBundle:Rucher','rucher')
+                ->getQuery()
+                ->getSingleScalarResult();
+    }    
 }
