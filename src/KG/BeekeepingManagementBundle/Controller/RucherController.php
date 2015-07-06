10e497e4dd2baadfd4ebf9a8822141995836ed2c
keverg <kevin.greneche@jack12.fr>
2015-07-06 15:17:23 +0200

    Mise en place des paramsconverter Evolution changement exploitation en dropdown

--- src/KG/BeekeepingManagementBundle/Controller/RucherController.php
+++ src/KG/BeekeepingManagementBundle/Controller/RucherController.php
@@ -15,41 +15,12 @@ class RucherController extends Controller
 {
     /**
     * @Security("has_role('ROLE_USER')")
-    */
-    public function indexAction($page)
-    {
-        if ($page < 1){
-            throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
-        }
-        
-        $maxRuchers     = $this->container->getParameter('max_ruchers_per_page');
-        $exploitationId = $this->getUser()->getExploitationEnCours()->getId();
-        $ruchers        = $this->getDoctrine()->getRepository('KGBeekeepingManagementBundle:Rucher')->getListByExploitation($page, $maxRuchers, $exploitationId);
-        $ruchers_count  = $this->getDoctrine()->getRepository('KGBeekeepingManagementBundle:Rucher')->countByExploitation($exploitationId); 
-        
-        $pagination = array(
-            'page'         => $page,
-            'route'        => 'kg_beekeeping_management_home_rucher',
-            'pages_count'  => max ( ceil($ruchers_count / $maxRuchers), 1),
-            'route_params' => array()
-        );
-        
-        return $this->render('KGBeekeepingManagementBundle:Rucher:index.html.twig', 
-                            array(  
-                                 'ruchers'      => $ruchers,
-                                 'nbRuchers'    => $ruchers_count,
-                                 'pagination'   => $pagination
-                            )
-        ); 
-    }
-
-    /**
-    * @Security("has_role('ROLE_USER')")
-    * @Route("/rucher/affichage-rucher/{rucher_id}")
-    * @ParamConverter("rucher", class="KGBeekeepingManagementBundle:Rucher", options={"repository_method" = "getRucherByExploitation"}) 
     */    
     public function viewAction(Rucher $rucher)
     {
+        if( $rucher->getExploitation() != $this->getUser()->getExploitationEnCours()){
+            throw new NotFoundHttpException('Page inexistante.');
+        }
         return $this->render('KGBeekeepingManagementBundle:Rucher:view.html.twig', array( 'rucher' => $rucher ));
     }
 
