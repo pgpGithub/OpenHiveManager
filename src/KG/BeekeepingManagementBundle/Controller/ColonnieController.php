a9087a1a3613b24fde68f8f7c17633c89b0bea57
keverg <kevin.greneche@jack12.fr>
2015-08-31 22:44:52 +0200

    Bug tuer colonnie

--- src/KG/BeekeepingManagementBundle/Controller/ColonnieController.php
+++ src/KG/BeekeepingManagementBundle/Controller/ColonnieController.php
@@ -240,6 +240,9 @@ class ColonnieController extends Controller
                 $colonnie->setMorte(true);
 
                 if( $colonnie->getRuche() ){
+                    if( $colonnie->getRuche()->getEmplacement() ){
+                        $colonnie->getRuche()->setEmplacement(null);
+                    }
                     $colonnie->getRuche()->setColonnie(null);
                 }
 
