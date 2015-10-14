3333c5fcc06346421d92f8e5942fadebdab925b4
keverg <kevin.greneche@jack12.fr>
2015-10-14 21:43:38 +0200

    Remerage

--- src/KG/BeekeepingManagementBundle/Form/EventListener/TypeRemerageFieldSubscriber.php
+++ src/KG/BeekeepingManagementBundle/Form/EventListener/TypeRemerageFieldSubscriber.php
@@ -17,24 +17,77 @@ class TypeRemerageFieldSubscriber implements EventSubscriberInterface
             FormEvents::PRE_SUBMIT    => 'preSubmit'
         );
     }
- 
-    private function addTypeRemerageForm($form, $disabled)
-    {
+    
+    
+    private function addTypeRemerageForm($form)
+    {  
         $form->add('naturel', 'checkbox', array(
-            'label' => false,
-            'required'  => false,
-            'disabled'  => $disabled
-        ));
+                   'label' => false,
+                   'required'  => false
+            ));    
+    }    
+ 
+    private function addOrRemoveForm($form, $naturel)
+    {  
+        if( !$naturel ){
+            $form->get('reine')->add('race', 'entity', array(
+                        'class' => 'KGBeekeepingManagementBundle:Race',
+                        'choice_label' => 'libelle',
+                        'empty_value' => '',
+                        'empty_data'  => null
+                    ))    
+                                ->add('anneeReine', 'collot_datetime', 
+                                        array( 
+                                                'pickerOptions' =>
+                                                    array('format' => 'yyyy',
+                                                        'autoclose' => true,
+                                                        'endDate'   => date('Y'), 
+                                                        'startView' => 'decade',
+                                                        'minView' => 'decade',
+                                                        'maxView' => 'decade',
+                                                        'todayBtn' => false,
+                                                        'todayHighlight' => false,
+                                                        'keyboardNavigation' => true,
+                                                        'language' => 'fr',
+                                                        'forceParse' => true,
+                                                        'pickerReferer ' => 'default', 
+                                                        'pickerPosition' => 'bottom-right',
+                                                        'viewSelect' => 'decade',
+                                                        'initialDate' => date('Y'), 
+                                                    ),
+                                                'read_only' => true
+                    ));         
+        }
+        else{
+            $form->get('reine')->remove('anneeReine');
+            $form->get('reine')->remove('race');
+        }        
     }
  
     public function preSetData(FormEvent $event)
     {
-        $this->addTypeRemerageForm($event->getForm(), $event->getData()->getNaturel());
+        $data = $event->getData();
+        $form = $event->getForm();
+        
+        if (null === $data) {
+            return;
+        }
+        
+        $this->addTypeRemerageForm($form);
+        
+        $accessor = PropertyAccess::createPropertyAccessor();
+        $naturel = $accessor->getValue($data, 'naturel');
+        
+        $this->addOrRemoveForm($form, $naturel);
     }
  
     public function preSubmit(FormEvent $event)
     {
-
-
+        $data = $event->getData();
+        $form = $event->getForm();
+        
+        $naturel = $data['naturel'];
+        
+        $this->addOrRemoveForm($form, $naturel);        
     }
 }
