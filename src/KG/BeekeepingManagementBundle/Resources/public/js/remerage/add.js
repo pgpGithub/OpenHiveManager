6790b906e06f8942140717ddde9af6ff64d2bff7
keverg <kevin.greneche@jack12.fr>
2015-10-14 14:54:31 +0200

    Remérage

--- src/KG/BeekeepingManagementBundle/Resources/public/js/remerage/add.js
+++ src/KG/BeekeepingManagementBundle/Resources/public/js/remerage/add.js
@@ -1,14 +1,2 @@
 $("[name='kg_beekeepingmanagementbundle_remerage[reine][clippage]']").bootstrapSwitch();
 $("[name='kg_beekeepingmanagementbundle_remerage[reine][marquage]']").bootstrapSwitch();
-$("[name='kg_beekeepingmanagementbundle_remerage[naturel]']").bootstrapSwitch();
-
-document.getElementById('kg_beekeepingmanagementbundle_remerage_naturel').addEventListener('click',function (event){
-    if(event.target.checked){
-        document.getElementById('kg_beekeepingmanagementbundle_remerage_reine_race').style.display = "inline-block";
-        document.getElementById('kg_beekeepingmanagementbundle_remerage_reine_anneeReine').style.display = "inline-block";
-    }else{
-        document.getElementById('kg_beekeepingmanagementbundle_remerage_reine_race').style.display = "none";
-        document.getElementById('kg_beekeepingmanagementbundle_remerage_reine_anneeReine').style.display = "none";
-    }
-});
-
