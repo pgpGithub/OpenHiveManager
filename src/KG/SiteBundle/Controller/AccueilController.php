4494e720f0c460a0325e8098585773d69e2d37cc
jack12 <kevin.greneche@jack12.fr>
2015-11-05 10:45:52 +0100

    Corrections première mise en prod

--- src/KG/SiteBundle/Controller/AccueilController.php
+++ src/KG/SiteBundle/Controller/AccueilController.php
@@ -21,33 +21,7 @@ class AccueilController extends Controller
     
     public function contactAction(Request $request)
     {          
-        $contact = new Contact();
-        
-        $form = $this->createForm(new ContactType(), $contact);
-        
-        if ($form->handleRequest($request)->isValid()){
-            $message = \Swift_Message::newInstance()
-                    ->setSubject($contact->getSujet()->getLibelle())
-                    ->setFrom($this->container->getParameter('mailer_user'))
-                    ->setBody($this->renderView('KGSiteBundle::mail.txt.twig', array('contact' => $contact)));
-            
-            if( $contact->getSujet()->getId() == 1 || $contact->getSujet()->getId() == 2){
-                $message->setTo($this->container->getParameter('support'));
-            }else{
-                $message->setTo($this->container->getParameter('contact'));
-            }
-            $this->get('mailer')->send($message);            
-            
-            $flash = $this->get('braincrafted_bootstrap.flash');
-            $flash->success('Message envoyé avec succès. Merci de nous avoir contacté.');
-            
-            return $this->redirect($this->generateUrl('kg_site_contact'));
-        }
-
-        return $this->render('KGSiteBundle:Accueil:contact.html.twig', 
-                             array(
-                                    'form'   => $form->createView()
-                ));        
+        return $this->render('KGSiteBundle:Accueil:contact.html.twig');        
     }    
 
     public function participerAction()
