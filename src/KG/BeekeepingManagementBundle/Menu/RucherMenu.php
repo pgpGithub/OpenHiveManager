312a4fc976daee14b79ab3f784d41cf3a1611f37
keverg <kevin.greneche@jack12.fr>
2015-11-11 09:43:01 +0100

    Génération QR code des ruches depuis le rucher

--- src/KG/BeekeepingManagementBundle/Menu/RucherMenu.php
+++ src/KG/BeekeepingManagementBundle/Menu/RucherMenu.php
@@ -60,7 +60,7 @@ class RucherMenu extends ContainerAware
 
         if( $qrcode_permitted ){
             $menu[$titleRucher]->addChild('.icon-qrcode QR Code', array(
-                'route' => 'kg_beekeeping_management_print_all_qr_code',
+                'route' => 'kg_beekeeping_management_view_all_qr_code',
                 'routeParameters' => array('rucher_id' => $rucher->getId())
             ));         
         }
