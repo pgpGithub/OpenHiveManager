0913415a42cc622fd29f877bfe7d99a7233b165a
Keverg <kevin.greneche@jack12.fr>
2016-01-14 10:12:11 +0100

    Refonte IHM

--- src/KG/BeekeepingManagementBundle/Resources/public/js/rucher/update.js
+++ src/KG/BeekeepingManagementBundle/Resources/public/js/rucher/update.js
@@ -15,4 +15,61 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-$("[name='kg_beekeepingmanagementbundle_rucher[numerotation]']").bootstrapSwitch();
+$(function() {
+
+
+    // Select2 select
+    // ------------------------------
+
+    // Basic
+    $('.select').select2();
+
+
+    //
+    // Select with icons
+    //
+
+    // Format icon
+    function iconFormat(icon) {
+        var originalOption = icon.element;
+        if (!icon.id) { return icon.text; }
+        var $icon = "<i class='icon-" + $(icon.element).data('icon') + "'></i>" + icon.text;
+
+        return $icon;
+    }
+
+    // Initialize with options
+    $(".select-icons").select2({
+        templateResult: iconFormat,
+        minimumResultsForSearch: Infinity,
+        templateSelection: iconFormat,
+        escapeMarkup: function(m) { return m; }
+    });
+
+
+
+    // Styled form components
+    // ------------------------------
+
+    // Checkboxes, radios
+    $(".styled").uniform({ radioClass: 'choice' });
+
+    // File input
+    $('.file-input-custom').fileinput({
+        previewFileType: 'image',
+        browseLabel: 'Select',
+        browseClass: 'btn bg-slate-700',
+        browseIcon: '<i class="icon-image2 position-left"></i> ',
+        removeLabel: 'Remove',
+        removeClass: 'btn btn-danger',
+        removeIcon: '<i class="icon-cancel-square position-left"></i> ',
+        uploadClass: 'btn bg-teal-400',
+        uploadIcon: '<i class="icon-file-upload position-left"></i> ',
+        layoutTemplates: {
+            icon: '<i class="icon-file-check"></i>'
+        },
+        initialCaption: "Please select image",
+        mainClass: 'input-group'
+    });   
+});
+
