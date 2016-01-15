0a91dabba5226b377d4e6587d50a59aba7cb256d
Keverg <kevin.greneche@jack12.fr>
2016-01-15 16:20:47 +0100

    Refonte IHM

--- src/KG/BeekeepingManagementBundle/Resources/public/js/visite/add.js
+++ src/KG/BeekeepingManagementBundle/Resources/public/js/visite/add.js
@@ -53,4 +53,31 @@ $(function() {
 
     // Checkboxes, radios
     $(".styled").uniform({ radioClass: 'choice' }); 
+    
+    
+    // Add hausse
+    // ------------------------------    
+    // keep track of how many hausses fields have been rendered
+    var hausseCount = '{{ form.hausses|length }}';
+
+    jQuery(document).ready(function() {
+        jQuery('#add-another-hausse').click(function(e) {
+            e.preventDefault();
+
+            var hausseList = jQuery('#hausse-fields-list');
+
+            // grab the prototype template
+            var newWidget = hausseList.attr('data-prototype');
+            // replace the "__name__" used in the id and name of the prototype
+            // with a number that's unique to your emails
+            // end name attribute looks like name="contact[emails][2]"
+            newWidget = newWidget.replace(/__name__/g, hausseCount);
+            hausseCount++;
+
+            // create a new list element and add it to the list
+            var newLi = jQuery('<div></div>').html(newWidget);
+            newLi.appendTo(hausseList);
+        });
+    })    
+    
 });
